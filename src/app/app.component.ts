66ddc9c0652b4402a2db69b7195b5a74d7796170
estee-m <105426615+estee-m@users.noreply.github.com>
2022-10-05 09:37:51 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { of, from, map, tap, take} from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +8,25 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit(){
+    from([20,15,10,5])
+    .pipe(
+      tap(item => console.log(`emitted item ... ${item}`)),
+      map(item => item * 2),
+      map(item => item - 10),
+      map(item => {
+        if(item === 0){
+          throw new Error('zero detected');
+        }
+        return item;
+      }),
+      take(3)
+    )
+    .subscribe({
+      next: (item) => console.log(`resulting item .. ${item}`),
+      error: (err) => console.error(`error occured ${err}`),
+      complete: () => console.log('complete'),
+    })
+  }
 }
